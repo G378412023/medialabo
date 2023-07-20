52e500182dbb84ef7757cdaf85accc82fef5930e
G378412023 <237841@st.takushoku-u.ac.jp>
2023-07-20 16:17:11 +0900

    変更

--- practice-js/weather.js
+++ practice-js/weather.js
@@ -1,116 +1,91 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather":[
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
 
-////////// 課題3-2 ここからプログラムを書こう
+//セレクトボタンがチェックされていたら、li要素を作成し、該当する情報を入れて返す
+function appendValue(root,number,text,data){
+  let t = document.form.weather;
+  if(t[number].checked){
+    let li = document.createElement("li");
+    li.textContent = text + data;
+    root.appendChild(li)
+  }
+}
 
-// h1北京
-let div = document.querySelector("div#result");
-let s1 = document.createElement("h1");
-s1.textContent = "北京市";
-div.appendChild(s1);
+// 通信を開始する処理
+function sendRequest() {
+  let id = document.getElementById("weather").value;
+  // URL を設定
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+id+'.json';
 
-// h2北京市の詳細
-let s2 = document.createElement("h2");
-s2.textContent = "北京市の詳細";
-div.appendChild(s2);
+  // 通信開始
+  axios.get(url)
+      .then(showResult)   // 通信成功
+      .catch(showError)   // 通信失敗
+      .then(finish);      // 通信の最後の処理
+}
 
-// ul要素緯度と経度
-let s3 = document.createElement("ul");
-let li6 = document.createElement("li");
-li6.textContent = "緯度："+data["coord"]["lon"];
-s3.appendChild(li6);
+// 通信が成功した時の処理
+function showResult(resp) {
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+  //国の名前を取得
+  let city = data["name"];
 
-let li7 = document.createElement("li");
-li7.textContent = "経度："+data["coord"]["lat"];
-s3.appendChild(li7);
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+  // data をコンソールに出力
+  console.log(data);
 
-div.appendChild(s3);
+  //前の都市の情報を削除
+  let element = document.getElementById("result");
+  element.innerHTML = "";
 
-// h2北京市の天気
-let s4 = document.createElement("h2");
-s4.textContent = "北京市の天気";
-div.appendChild(s4);
+  // h1北京
+  let div = document.querySelector("div#result");
+  let s1 = document.createElement("h1");
+  s1.textContent = city;
+  div.appendChild(s1);
 
-//ul要素、最低気温、最高気温、湿度、風速、風向
-let s5 = document.createElement("ul");
-
-let li1 = document.createElement("li");
-li1.textContent = "最低気温: "+data["main"]["temp_min"];
-s5.appendChild(li1)
-
-let li2 = document.createElement("li");
-li2.textContent = "最高気温: "+data["main"]["temp_max"];
-s5.appendChild(li2)
+  // h2北京市の詳細
+  let s2 = document.createElement("h2");
+  s2.textContent = city+"の詳細";
+  div.appendChild(s2);
 
 
-let li3 = document.createElement("li");
-li3.textContent = "湿度: "+data["main"]["humidity"];
-s5.appendChild(li3)
+  // ul要素緯度と経度
+  let s3 = document.createElement("ul");
 
-let li4 = document.createElement("li");
-li4.textContent = "風速: "+data["wind"]["speed"];
-s5.appendChild(li4)
+  appendValue(s3,1,"緯度：",data["coord"]["lon"]);
+  appendValue(s3,2,"経度：",data["coord"]["lat"]);
 
-let li5 = document.createElement("li");
-li5.textContent = "風向: "+data["wind"]["deg"];
-s5.appendChild(li5)
+  div.appendChild(s3);
 
-div.appendChild(s5);
+  // h2北京市の天気
+  let s4 = document.createElement("h2");
+  s4.textContent = city+"の天気";
+  div.appendChild(s4);
 
+  //ul要素、最低気温、最高気温、湿度、風速、風向
+  let s5 = document.createElement("ul");
 
+  appendValue(s5,3,"最高気温：",data["main"]["temp_max"]);
+  appendValue(s5,4,"最低気温：",data["main"]["temp_min"]);
+  appendValue(s5,5,"湿度：",data["main"]["humidity"]);
+  appendValue(s5,6,"風速：",data["wind"]["speed"]);
+  appendValue(s5,7,"風向：",data["wind"]["deg"]);
 
+  div.appendChild(s5);
 
+}
 
-//課題5-2
-let b = document.querySelector("#button");
-b.addEventListener("click",search);
+// 通信エラーが発生した時の処理
+function showError(err) {
+  console.log(err);
+}
 
-function search(){
-  let f = document.querySelector("#form");
-  let r = f.querySelector('input[name="weather"]:checked')
-  console.log(r.value);
+// 通信の最後にいつも実行する処理
+function finish() {
+  console.log('Ajax 通信が終わりました');
 }
